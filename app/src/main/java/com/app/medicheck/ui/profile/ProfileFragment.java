e4a5f1a05c5d6087e57f772120dd44227b4c0824
Oļģerts <35864921+figiter771@users.noreply.github.com>
2019-12-10 15:51:40 +0200

    Added actionBar to profile fragment
    Added custom Toolbar to fragment profile.

--- app/src/main/java/com/app/medicheck/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/app/medicheck/ui/profile/ProfileFragment.java
@@ -1,18 +1,26 @@
 package com.app.medicheck.ui.profile;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.TooltipCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.app.medicheck.MainActivity;
 import com.app.medicheck.R;
 
 public class ProfileFragment extends Fragment {
@@ -31,13 +39,34 @@ public class ProfileFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+
+
         return root;
     }
     public void onResume() {
         super.onResume();
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         if (activity != null && activity.getSupportActionBar() != null) {
-            activity.getSupportActionBar().show();
+            activity.getSupportActionBar().hide();
+            createActionBar();
         }
     }
+
+    public void createActionBar () {
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        androidx.appcompat.widget.Toolbar profileToolbar =(Toolbar) activity.findViewById(R.id.fragment_profile_toolbar);
+        profileToolbar.inflateMenu(R.menu.profile_toolbar_menu);
+        profileToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast ToolbarToast = Toast.makeText(getContext(),item.getTitle(),Toast.LENGTH_SHORT);
+                ToolbarToast.show();
+                return false;
+            }
+        });
+    }
+
+
 }
